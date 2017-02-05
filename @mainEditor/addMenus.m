566042106b851e1742c1afcdc1da82135c1c8340
ahmettaskin <ahmet.taskin@gkfx.com>
2017-02-05 18:07:51 +0300

    clean-up.

--- @mainEditor/addMenus.m
+++ @mainEditor/addMenus.m
@@ -104,8 +104,8 @@ function obj = saveWs(~, ~, obj)
 
 %% Load t2fis
 hFig = findall(0,'tag','fuzzyt2');
-t2fis=get(hFig,'userdata');
-t2fis=t2fis{1};
+t2fis=helper.getAppdata();
+
 answer=inputdlg({'name of t2fis'},'name',1,{'t2fis'});
 drawnow;
 if isempty(answer)
